ea2b600887d4be99fc82ea86b7f145ae2018a414
jolebowski <jordan.moreira@epitech.eu>
2021-10-14 14:57:54 +0200

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-type appProps = {
-  headerText: string,
-  subtitle?:string
-}
-function App({headerText, subtitle = "default text"}: appProps) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{headerText}</h1>
-        {subtitle && <h2>{subtitle}</h2>}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
