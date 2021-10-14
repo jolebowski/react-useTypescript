c4d389b4325c0f160efc2d662461068149497385
jolebowski <jordan.moreira@epitech.eu>
2021-10-14 15:55:45 +0200

    state-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+type adress = {
+  city: string,
+  number: number,
+  street: string
+}
+
+type User = {
+  name:string,
+  age: number,
+  adress: adress
+}
+
 function App() {
+  const [user, setUser] = useState<User | null>(null)
+  
+  const fetchUser = () => 
+    setUser({
+      name: "Jordan",
+      age: 28, 
+      adress: {
+      city:"paris",
+      number: 89,
+      street:"jean boulevard"
+      },
+    })
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <h1>Header</h1>
+         <button onClick={fetchUser}>fetch user on click</button>    
+         {user && <p>{`Welcome ${user.name}`}</p>}      
       </header>
     </div>
   );
